6cf162838ddf2a4cb57bcefb5d175f3cf2e1436b
Kuba Matjanowski <k.matjanowski@gmail.com>
2018-04-16 00:19:04 +0200

    remove from list on close

--- 2_angular/main.ts
+++ 2_angular/main.ts
@@ -58,14 +58,14 @@ function createWindow() {
     }));
   }
 
-  win.webContents.openDevTools();
-
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null;
+    let index = windows.indexOf(win);
+    windows.splice(index, 1);
   });
   windows.push(win);
 }
